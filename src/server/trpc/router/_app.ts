a87f1889a5b6818ed831405d76966a815004d9fe
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2022-12-29 00:50:01 -0500

    Add authRouter

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { exampleRouter } from "./example";
 import { recipesRouter } from "./recipes/recipesRouter";
+import { authRouter } from "./auth/authRouter";
 
 export const appRouter = router({
   example: exampleRouter,
   recipes: recipesRouter,
+  auth: authRouter,
 });
 
 // export type definition of API
