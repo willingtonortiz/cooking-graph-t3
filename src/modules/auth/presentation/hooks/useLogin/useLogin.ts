8869be184fd0dd9cdbec3be49caa548d2db113ab
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2023-01-02 15:37:06 -0500

    Save token on login in useLogin

--- src/modules/auth/presentation/hooks/useLogin/useLogin.ts
+++ src/modules/auth/presentation/hooks/useLogin/useLogin.ts
@@ -2,5 +2,12 @@ import { trpc } from "../../../../../utils/trpc";
 import type { UseLoginProps } from "./useLogin.types";
 
 export const useLogin = ({ onSuccess, onError }: UseLoginProps) => {
-  return trpc.auth.login.useMutation({ retry: false, onSuccess, onError });
+  return trpc.auth.login.useMutation({
+    retry: false,
+    onSuccess: ({ token }) => {
+      localStorage.setItem("token", token);
+      onSuccess?.();
+    },
+    onError,
+  });
 };
