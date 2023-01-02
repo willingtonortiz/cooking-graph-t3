0cc921d33809eeeb40a31781dc6a8b445baeb3cd
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2023-01-02 15:09:41 -0500

    Add jwt service jsonwebtoken implementation

---
+++ src/server/modules/auth/infrastructure/services/jsonwebtoken-jwt-service.ts
@@ -0,0 +1,19 @@
+import type { JwtService } from "../../domain/services/jwt-service";
+import type { JwtPayload } from "../../domain/interfaces/jwt-payload";
+import { sign as signToken, verify as verifyToken } from "jsonwebtoken";
+
+export class JsonWebTokenJwtService implements JwtService {
+  sign(payload: JwtPayload): string {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return signToken(payload, process.env.JWT_SECRET!, { expiresIn: "5m" });
+  }
+
+  verify(token: string): JwtPayload | null {
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return verifyToken(token, process.env.JWT_SECRET!) as JwtPayload;
+    } catch (e) {
+      return null;
+    }
+  }
+}
