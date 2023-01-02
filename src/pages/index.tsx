6cec8c20dcdd659f06f36c1102a657a32d18d24a
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2023-01-02 17:56:15 -0500

    Remove comments from "/"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,6 @@ import { type NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
-  // const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
