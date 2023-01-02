36dfb7051b8b207e3cf63d9e375070784054e92f
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2023-01-02 15:37:58 -0500

    Remove example router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { recipesRouter } from "./recipes/recipesRouter";
 import { authRouter } from "./auth/authRouter";
 
 export const appRouter = router({
-  example: exampleRouter,
-  recipes: recipesRouter,
   auth: authRouter,
+  recipes: recipesRouter,
 });
 
 // export type definition of API
