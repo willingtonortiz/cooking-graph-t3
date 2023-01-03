b8ec6a6c827efca07336e2f8676748df7439ef99
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2023-01-03 09:46:00 -0500

    Update typings and remove comments

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -1,15 +1,10 @@
 import { type inferAsyncReturnType } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest } from "next";
 
 import { prisma } from "../db/client";
 import type { JwtPayload } from "../modules/auth/domain/interfaces/jwt-payload";
 import { JsonWebTokenJwtService } from "../modules/auth/infrastructure/services/jsonwebtoken-jwt-service";
-
-type CreateContextOptions = {
-  req: NextApiRequest;
-  res: NextApiResponse;
-};
 
 const getUserFromRequest = (req: NextApiRequest): JwtPayload | null => {
   const token = req.cookies.token;
@@ -21,7 +16,7 @@ const getUserFromRequest = (req: NextApiRequest): JwtPayload | null => {
   return jwtService.verify(token);
 };
 
-export const createContextInner = async (opts: CreateContextOptions) => {
+export const createContextInner = async (opts: CreateNextContextOptions) => {
   const user = getUserFromRequest(opts.req);
 
   return { ...opts, prisma, user };
